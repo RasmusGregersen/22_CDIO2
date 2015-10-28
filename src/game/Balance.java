e2be873fa6b484f8f2cecb55f02893d62ac6899a
Hannibal Moulvad <Hannibal Moulvad>
2015-10-28 11:32:08 +0100

    Balance class..... Bøgse engelsk

--- src/game/Balance.java
+++ src/game/Balance.java
@@ -2,4 +2,19 @@ package game;
 
 public class Balance {
 
+	private int balance = 1000;
+
+	public void setBalance(int balance) {
+
+		if (balance<0)
+			setBalance(0);
+		else
+			this.balance = balance;
+
+	}
+
+	public int getBalance()	{
+		return balance;
+	}
+
 }
