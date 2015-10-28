72c4cdf8b747273f58dcd245115d2c0f4a2a14d5
Hannibal Moulvad <Hannibal Moulvad>
2015-10-28 12:05:34 +0100

    Added attributes to the playerclass

--- src/game/Player.java
+++ src/game/Player.java
@@ -8,32 +8,25 @@
 package game;
 
 public class Player {
-	private boolean doublesix = false; // counter for doublesix
 	private String name; // player name
-	private int point = 0; // player points, starting in 0.
+	private int fieldloc = 1; // field location
 	
-	// method to return doublesix counter.
-	public boolean getDoublesix() {
-		return doublesix;
-	}
 	// method to return player name.
 	public String getName()	{
 		return name;
-	}
-	// method to return player point.
-	public int getPoint() {
-		return point;
-	}
-	// method to set doublesix counter.
-	public void setDoublesix(boolean doublesix) {
-		this.doublesix = doublesix;
 	}
 	// method to set player name.
 	public void setName(String name) {
 		this.name = name;
 	}
-	// method to set player point.
-	public void setPoint(int point) {
-		this.point = point;
+	
+	// method to return players fieldlocation.
+	public int getFieldloc() {
+		return fieldloc;
+	}
+	
+	// method to return players fieldlocation.
+	public void setFieldloc(int fieldloc) {
+		this.fieldloc = fieldloc;
 	}
 }
