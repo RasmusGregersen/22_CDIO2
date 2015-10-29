ee2d2afde11ca6eb570f2f25cbbfd163881ed95d
Hannibal Moulvad <Hannibal Moulvad>
2015-10-29 17:53:58 +0100

    Added language messages to the Game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -20,6 +20,7 @@ public class Game {
 		DiceCup dicecup = new DiceCup();
 		Player player1 = new Player();
 		Player player2 = new Player();
+		Language language = new Language();
 		int fields = 0;
 
 		// set player names from GUI input.
@@ -62,70 +63,94 @@ public class Game {
 				GUI.removeAllCars(player1.getName());
 				fields = dicecup.getSum();
 				GUI.setCar(fields - 1, player1.getName());
-				switch (fields) {
+				GUI.showMessage(player1.getName() + " rolled a " + dicecup.getDie1() + " and " + dicecup.getDie2());
+				
+				switch (fields - 1) {
 				case 2:  player1.setBalance(player1.getBalance() + 250);
+				GUI.showMessage(language.getField1());
 				break;
 				case 3:  player1.setBalance(player1.getBalance() - 200);
+				GUI.showMessage(language.getField2());
 				break;
 				case 4:  player1.setBalance(player1.getBalance() - 100);
+				GUI.showMessage(language.getField3());
 				break;
 				case 5:  player1.setBalance(player1.getBalance() - 20);
+				GUI.showMessage(language.getField4());
 				break;
 				case 6:  player1.setBalance(player1.getBalance() + 180);
+				GUI.showMessage(language.getField5());
 				break;
 				case 7:  player1.setBalance(player1.getBalance() + 0);
+				GUI.showMessage(language.getField6());
 				break;
 				case 8:  player1.setBalance(player1.getBalance() - 70);
+				GUI.showMessage(language.getField7());
 				next = player1;
 				break;
 				case 9:  player1.setBalance(player1.getBalance() - 60);
+				GUI.showMessage(language.getField8());
 				break;
 				case 10: player1.setBalance(player1.getBalance() - 80);
+				GUI.showMessage(language.getField9());
 				break;
 				case 11: player1.setBalance(player1.getBalance() - 90);
+				GUI.showMessage(language.getField10());
 				break;
 				case 12: player1.setBalance(player1.getBalance() + 650);
+				GUI.showMessage(language.getField11());
 				break;
 				}
 				GUI.setBalance(player1.getName(), player1.getBalance());
 				// Prints out player1's roll
-				GUI.showMessage(player1.getName() + " rolled a " + dicecup.getDie1() + " and " + dicecup.getDie2());
-
+				
 
 			}
-			// Repeats same if/else for player 2.
 			else {
 				next = player1;
 				GUI.removeAllCars(player2.getName());
 				fields = dicecup.getSum();
 				GUI.setCar(fields - 1, player2.getName());
+				GUI.showMessage(player2.getName() + " rolled a " + dicecup.getDie1() + " and " + dicecup.getDie2());
+				
 				switch (fields - 1) {
 				case 2:  player2.setBalance(player2.getBalance() + 250);
+				GUI.showMessage(language.getField1());
 				break;
 				case 3:  player2.setBalance(player2.getBalance() - 200);
+				GUI.showMessage(language.getField2());
 				break;
 				case 4:  player2.setBalance(player2.getBalance() - 100);
+				GUI.showMessage(language.getField3());
 				break;
 				case 5:  player2.setBalance(player2.getBalance() - 20);
+				GUI.showMessage(language.getField4());
 				break;
 				case 6:  player2.setBalance(player2.getBalance() + 180);
+				GUI.showMessage(language.getField5());
 				break;
 				case 7:  player2.setBalance(player2.getBalance() + 0);
+				GUI.showMessage(language.getField6());
 				break;
 				case 8:  player2.setBalance(player2.getBalance() - 70);
+				GUI.showMessage(language.getField7());
 				next = player2;
 				break;
 				case 9:  player2.setBalance(player2.getBalance() - 60);
+				GUI.showMessage(language.getField8());
 				break;
 				case 10: player2.setBalance(player2.getBalance() - 80);
+				GUI.showMessage(language.getField9());
 				break;
 				case 11: player2.setBalance(player2.getBalance() - 90);
+				GUI.showMessage(language.getField10());
 				break;
 				case 12: player2.setBalance(player2.getBalance() + 650);
+				GUI.showMessage(language.getField11());
 				break;
 				}
 				GUI.setBalance(player2.getName(), player2.getBalance());
-				GUI.showMessage(player2.getName() + " rolled a " + dicecup.getDie1() + " and " + dicecup.getDie2());
+				
 				
 			}
 
