ba77029c9e786d122f244d86327f4ae28480ab30
Hannibal Moulvad <Hannibal Moulvad>
2015-10-30 12:11:54 +0100

    Fixed Balance.class

--- src/game/Balance.java
+++ src/game/Balance.java
@@ -5,19 +5,22 @@ public class Balance {
 	private int balance = 1000;
 
 	public void setBalance(int balance) {
-			this.balance = balance;
+		this.balance = balance;
 	}
 	public void withdraw(int balance) {
 		this.balance = this.balance - balance;
-		if (balance<0)
-			setBalance(0);
 	}
 	public void deposit(int balance) {
 		this.balance = this.balance + balance;
-		if (balance>3000)
-			setBalance(3000);
+
 	}
 	public int getBalance()	{
+		if (balance>3000)
+			setBalance(3000);
+
+		else if (balance<0)
+			setBalance(0);
+
 		return balance;
 	}
 }
