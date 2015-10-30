fa725718235efe4a25ad8c755f8c626e95dd8458
Rasmus Gregersen <rasmus.gregersen@live.dk>
2015-10-30 14:11:49 +0100

    Test class updated!!

--- src/test/TestNegativeBalance.java
+++ src/test/TestNegativeBalance.java
@@ -6,25 +6,29 @@ public class TestNegativeBalance {
 
 	public static void main(String[] args) {
 		Balance Balance = new Balance();
+		
+		
+		// Deposit test 10 times with random numbers
+		for (int i=0; i < 10; i++)
+		{
+			int dep = (int)Math.ceil(Math.random()*10000);
+			Balance.withdraw(3000);
+			System.out.println("Balance reset to 0!");
+			System.out.println("Trying to deposit: " + dep);
+			Balance.deposit(dep);
+			System.out.println("Balance is now: " + Balance.getBalance());
+		}
 
-		Balance.setBalance(0);
-		
-		Balance.withdraw(1500);
-
-		System.out.println("We set the balance to 0, and try to withdraw 1500 from it. If our method works the Current balance should be equal to 0, " + "Current balance = " + Balance.getBalance());
-		
-		Balance.setBalance(0);
-		
-		Balance.deposit(50000);
-		
-		System.out.println("We set the balance to 0, and try to deposit 50000 to it. If our method works the Current balance should not be greater than 3000 which is the winning condition, " + "Current balance = " + Balance.getBalance());
-		
-		Balance.setBalance(0);
-		
-		Balance.deposit(1000);
-		
-		System.out.println("We set the balance to 0, and try to deposit 1000 to it. If our method works the Current balance should be equal to 1000, " + "Current balance = " + Balance.getBalance());
-			
+		// Withdraw test 10 times with random numbers
+		for (int i=0; i < 10; i++)
+		{
+			int wit = (int)Math.ceil(Math.random()*10000);
+			Balance.deposit(3000);
+			System.out.println("Balance reset to 3000!");
+			System.out.println("Trying to withdraw: " + wit);
+			Balance.withdraw(wit);
+			System.out.println("Balance is now: " + Balance.getBalance());
+		}		
 	}
 
 }
